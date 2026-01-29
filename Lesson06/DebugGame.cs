250e21b479ff962ff83748d0ff035d3562efb126
AgentVen <agentven0x@gmail.com>
2026-01-29 10:10:30 -0700

    Renamed Game1 to DebugGame

--- Lesson06/DebugGame.cs
+++ Lesson06/DebugGame.cs
@@ -4,12 +4,12 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Lesson06;
 
-public class Game1 : Game
+public class DebugGame : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
-    public Game1()
+    public DebugGame()
     {
         _graphics = new GraphicsDeviceManager(this);
         Content.RootDirectory = "Content";
--- Lesson06/Program.cs
+++ Lesson06/Program.cs
@@ -1,2 +1,2 @@
-﻿using var game = new Lesson06.Game1();
+﻿using var game = new Lesson06.DebugGame();
 game.Run();
